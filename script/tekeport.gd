9c850141e1940a2fc3d37d39f346545e5ff73c93
Piyush Adhikari <piyushadhikari437@gmail.com>
2025-09-26 14:24:29 +0530

    start

--- script/tekeport.gd
+++ script/tekeport.gd
@@ -1,20 +1,7 @@
 extends Area2D
 
-# The place where the player will be teleported
-@export var target_position: Vector2
-
-# Optional: Pull strength for a nice “suction” effect
-@export var pull_strength: float = 300.0
-
-func _process(delta):
-	for body in get_overlapping_bodies():
-		if body.is_in_group("player"):
-			var direction = (global_position - body.global_position).normalized()
-			var distance = body.global_position.distance_to(global_position)
-			
-			# Pull player toward blackhole
-			body.position += direction * pull_strength * delta / distance
-			
-			# Teleport when close enough
-			if distance < 10:
-				body.global_position = target_position
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.set_position($Destination.global_position)
+		
+	
